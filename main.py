9dca8e14e52d8f442228ad06783f25c5f1691194
patelharsh29 <145406975+patelharsh29@users.noreply.github.com>
2025-12-18 16:29:57 -0500

    Project Main Files

---
+++ main.py
@@ -0,0 +1,59 @@
+import streamlit as st 
+import numpy as np 
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+
+model = load_model("skin_cancer_cnn.h5")
+
+# function to preprocess and predict the image
+def predict_skin_cancer(image_path, model):
+    img = image.load_img(image_path, target_size=(224, 224))  # Load Image
+    img_array = image.img_to_array(img) / 255.0  # Normalize
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+
+    # Make Prediction
+    prediction = model.predict(img_array)
+    class_label = "Malignant" if prediction > 0.5 else "Benign"
+
+    return class_label, img
+
+
+# streamlit application code
+st.title("Skin Cancer Detection System")
+
+st.markdown("""
+    This is a skin cancer detection system application. Upload an image, and the model will predict 
+    whether the skin lesion is **Malignant** or **Benign**.
+""")
+
+# file uploader
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_image is not None:
+    # predicting and displaying results
+    class_label, img = predict_skin_cancer(uploaded_image, model)
+
+    # displaying the result image with the prediction result
+    st.image(uploaded_image, caption="Uploaded Image", use_container_width=True)  # Show the uploaded image first
+    st.write(f"**Prediction: {class_label}**")
+
+    # # displaying processed result image with the prediction result
+    # fig, ax = plt.subplots()
+    # ax.imshow(img)
+    # ax.set_title(f"Predicted: {class_label}")
+    # ax.axis("off")
+    # st.pyplot(fig)
+
+st.markdown("""
+    ### About the Model:
+    This model uses 2D CNN architecture for predicting whether a skin lesion 
+    is **Benign** or **Malignant** based on images of skin lesions.
+
+    #### Features:
+    - **Input**: Skin lesion images
+    - **Output**: **Benign** or **Malignant** classification
+
+    #### How to use:
+    1. Upload an image of a skin lesion.
+    2. The model will predict if it's **Benign** or **Malignant**.
+""")
